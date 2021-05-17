e81236665d4986c5f97d68492ea9c0e379fd8b7d
Igor Vujovic <igor.vujovic@Igor-MDM.local>
2021-05-17 09:13:59 +0200

    prefetch

--- src/routes/project/[construction_id]/__layout.svelte
+++ src/routes/project/[construction_id]/__layout.svelte
@@ -16,7 +16,7 @@
 <div class="container">
     <ul class="nav nav-pills nav-fill mt-2" style="font-size: 0.7rem!important;">
         <li class="nav-item">
-            <a class:active={pagename == 'timesheet'} class="nav-link" aria-current="page" href="/project/{$page.params.construction_id}/timesheet/ljudski">
+            <a sveltekit:prefetch class:active={pagename == 'timesheet'} class="nav-link" aria-current="page" href="/project/{$page.params.construction_id}/timesheet/ljudski">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar3" viewBox="0 0 16 16">
                     <path d="M14 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM1 3.857C1 3.384 1.448 3 2 3h12c.552 0 1 .384 1 .857v10.286c0 .473-.448.857-1 .857H2c-.552 0-1-.384-1-.857V3.857z"/>
                     <path d="M6.5 7a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
@@ -25,7 +25,7 @@
             </a>
         </li>
         <li class="nav-item">
-            <a class:active={pagename == 'pracenje'} class="nav-link" href="/project/{$page.params.construction_id}/pracenje">
+            <a sveltekit:prefetch class:active={pagename == 'pracenje'} class="nav-link" href="/project/{$page.params.construction_id}/pracenje">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard-check" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
                     <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
@@ -34,7 +34,7 @@
                 Pracenje
             </a>
         </li>
-        <li class="nav-item">
+        <li sveltekit:prefetch class="nav-item">
             <a class:active={pagename == 'dnevnik'} class="nav-link" href="/project/{$page.params.construction_id}/dnevnik/1">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-layout-text-sidebar" viewBox="0 0 16 16">
                     <path d="M3.5 3a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 3a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zM3 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5z"/>
--- src/routes/project/[construction_id]/timesheet/__layout.svelte
+++ src/routes/project/[construction_id]/timesheet/__layout.svelte
@@ -8,16 +8,16 @@
 <div class="container">
     <ul class="nav nav-tabs nav-fill mb-2" style="font-size: 0.7rem!important;">
         <li class="nav-item">
-        <a class:active={pagename == 'ljudski'} class="nav-link" aria-current="page" href="/project/{$page.params.construction_id}/timesheet/ljudski">Ljudski resursi</a>
+        <a sveltekit:prefetch class:active={pagename == 'ljudski'} class="nav-link" aria-current="page" href="/project/{$page.params.construction_id}/timesheet/ljudski">Ljudski resursi</a>
         </li>
         <li class="nav-item">
-        <a class:active={pagename == 'vozila'} class="nav-link" href="/project/{$page.params.construction_id}/timesheet/vozila">Vozila</a>
+        <a sveltekit:prefetch class:active={pagename == 'vozila'} class="nav-link" href="/project/{$page.params.construction_id}/timesheet/vozila">Vozila</a>
         </li>
         <li class="nav-item">
-        <a class:active={pagename == 'masine'} class="nav-link" href="/project/{$page.params.construction_id}/timesheet/masine">Masine</a>
+        <a sveltekit:prefetch class:active={pagename == 'masine'} class="nav-link" href="/project/{$page.params.construction_id}/timesheet/masine">Masine</a>
         </li>
         <li class="nav-item">
-        <a class:active={pagename == 'materijal'} class="nav-link" href="/project/{$page.params.construction_id}/timesheet/materijal" >Materijal</a>
+        <a sveltekit:prefetch class:active={pagename == 'materijal'} class="nav-link" href="/project/{$page.params.construction_id}/timesheet/materijal" >Materijal</a>
         </li>
     </ul>
 </div>  
